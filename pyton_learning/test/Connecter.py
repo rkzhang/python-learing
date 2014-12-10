f5f10f9806ccb52b295d6632f22ada259d202005
rkzhang <zhangrongk@126.com>
2014-12-10 22:33:40 +0800

    connecter

---
+++ pyton_learning/test/Connecter.py
@@ -0,0 +1,12 @@
+'''
+@author: zhangr01
+'''
+
+import pymongo
+connection = pymongo.Connection('10.1.132.166', 22222)
+db = connection.test
+    
+db.user.insert({'id': '123','name': 'name' ,'sex': 'man', 'info' : "{ 'id' : 1, 'name' : 'rkz' }" }) 
+
+for rec in db.user.find() :
+    print rec['info']
